ee24022fccc6bfbfc7bb5d532c0fee3175d97cd9
Catherine Karra and kate hall <catherine@>
2016-03-24 16:57:27 -0700

    finished adding if else if statements in js to calculate 4 functions

--- js/scripts.js
+++ js/scripts.js
@@ -15,35 +15,24 @@ var divide = function(number1, number2) {
 };
 
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
+  $("form#calculator").submit(function(event) {
     event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
-    var result = add(number1, number2);
-    $("#output1").text(result);
-  });
-
-  $("form#subtract").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#subtract1").val());
-    var number2 = parseInt($("#subtract2").val());
-    var result = subtract(number1, number2);
-    $("#output2").text(result);
-  });
-
-  $("form#multiply").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#multiply1").val());
-    var number2 = parseInt($("#multiply2").val());
-    var result = multiply(number1, number2);
-    $("#output3").text(result);
-  });
-
-  $("form#divide").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#divide1").val());
-    var number2 = parseInt($("#divide2").val());
-    var result = divide(number1, number2);
-    $("#output4").text(result);
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+    var result
+    if (operator === "add"){
+      $("#answer").text(add(number1, number2));
+      $("#output").show();
+    } else if (operator === "subtract"){
+      $("#answer").text(subtract(number1, number2));
+      $("#output").show();
+    } else if (operator === "multiply"){
+      $("#answer").text(multiply(number1, number2));
+      $("#output").show();
+    } else {
+      $("#answer").text(divide(number1, number2));
+      $("#output").show();
+    }
   });
 });
